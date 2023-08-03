d84a4848ac2f5f8072bcbd59405248a0ae4f57c7
kyungtae <a42230@naver.com>
2023-08-03 16:15:51 +0900

    java

---
+++ src/chap_01/_01_String1.java
@@ -0,0 +1,18 @@
+package chap_01;
+
+import java.util.Locale;
+
+public class _01_String1 {
+    public static void main(String[] args) {
+        String s = "I like Java and Python and C.";
+        System.out.println(s);
+        //문자열의 길이
+        System.out.println(s.length());//29
+        //대소문자 변환
+        System.out.println(s.toUpperCase());
+        //소문자 변환
+        System.out.println(s.toLowerCase());
+        //포함관계
+        System.out.println(s.contains("Java"));
+    }
+}
