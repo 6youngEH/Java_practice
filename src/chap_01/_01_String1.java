d28b29098d1a0f992eb996de208bb9c21f44a454
kyungtae <a42230@naver.com>
2023-08-07 15:12:36 +0900

    String

--- src/chap_01/_01_String1.java
+++ src/chap_01/_01_String1.java
@@ -16,6 +16,11 @@ public class _01_String1 {
         System.out.println(s.contains("Java"));//포함된다면 true , 포함되지 않는다면 false
         System.out.println(s.contains("C#"));//false
         System.out.println(s.indexOf("Java"));//위치 정보 확인
+        System.out.println(s.indexOf("C#"));//포함되지 않는다면 -1
+        System.out.println(s.indexOf("and"));//처음 일치하는 위치정보
+        System.out.println(s.lastIndexOf("and"));// 마지막에 일치하는 위치정보
+        System.out.println(s.startsWith("I like"));// 이 문자열로 시작하면 true (아니면 false)
+        System.out.println(s.endsWith("."));//이 문자열로 끝나면 true (아니면 false)
 
     }
 }
